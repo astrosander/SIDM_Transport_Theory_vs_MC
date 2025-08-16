e30dc2d58e972fa7823dd20229ef44e24144cc00
Aliaksandr Melnichenka <togpe22@gmail.com>
2025-08-16 15:43:56 -0400

    Additional plot of collision probability v*sigma(v,theta)

--- sigma_from_theta/sigma_theta - integration.py
+++ sigma_from_theta/sigma_theta - integration.py
@@ -38,4 +38,23 @@ plt.title('Cross Section vs Scattering Angle')
 plt.legend()
 plt.grid(True)
 plt.savefig("sigma_theta.png", dpi=160)
-plt.show()
+# plt.show()
+
+plt.figure(figsize=(8,6))
+
+for x in np.concatenate(([0], np.logspace(0, 1, num=10, base=10))):
+    v = x / 2
+
+    dsigma_vals = np.linspace(0, 0, 500)
+    for i in range(500):
+        dsigma_vals[i] = sigma(theta_vals[i], v)*v
+
+    plt.plot(theta_vals, dsigma_vals, label=rf'$\frac{{v}}{{\omega}} = $ {v/w:.1f}')
+
+plt.xlabel(r'$\theta$ (rad)')
+plt.ylabel(r'$v\cdot\sigma(v,\theta)$')
+plt.title(r'Collision probability $v\cdot\sigma(v,\theta)$ vs Scattering Angle')
+plt.legend()
+plt.grid(True)
+plt.savefig("sigma_thetaV.png", dpi=160)
+# plt.show()
