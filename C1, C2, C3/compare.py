3a9a98334b03ae4232d9a4e641ba8eaacf8e83c6
Aliaksandr Melnichenka <togpe22@gmail.com>
2025-09-03 23:04:53 -0400

    Succefully aligned C1 analytical/simulational values

--- C1, C2, C3/compare.py
+++ C1, C2, C3/compare.py
@@ -1,56 +1,38 @@
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy.integrate import quad
+import matplotlib.pyplot as plt
 
-w = 1.0
-A1 = np.linspace(0.05, 6.0, 400)  # let A1 = r = v/w
-r = A1
-v = r * w
-Ab = 0.5 * v**2 + w**2
-Bb = 0.5 * v**2
+r_vals = np.linspace(0.05, 60.0, 400)
 
-# C1_big_form2 = np.log(1.0 - b) - np.log(1.0 + b) + 1.0/(1.0 - b) - 1.0/(1.0 + b)
-
-
-# R = -np.log(1.0 + 2.0*a)
-
-# C01 = Ab/(Bb**2) * (1/(Ab-Bb)-1/(Ab+Bb))+1/(Bb**2)*np.log((Ab-Bb)/(Ab+Bb)) * (Bb*Bb - Ab*Ab)/2
-
-C01 = 2/(Bb**2-Ab**2)
-
-# C01 = 1/(Bb**2) * np.log((Ab-Bb)/(Ab+Bb)) - 2*Ab/Bb/(Bb**2-Ab**2)
-# C01 = C01 * (Bb**2-Ab**2) / 2
-
-# C01 = Ab/Bb+(Ab**2-Bb**2)/(2*Bb**2)*np.log((Ab-Bb)/(Ab+Bb))
-#2/(Bb**2-Ab**2)
-
-# C01 = 1/(Bb**2)*(np.log(1.0 - b) - np.log(1.0 + b))-Ab/Bb*2/(Bb**2-Ab**2
-# b = 
-# C01 = np.log((Ab - Bb)/(Ab + Bb)) + 2*Ab*Bb/(Ab**2-Bb**2)
-
-
-# R = np.log(np.clip((Ab - Bb) / (Ab + Bb), 1e-300, 1.0))
-# C1_big_form1 = 1.0       + (Ab * (Ab*Ab - Bb*Bb) / (Bb**3)) * R
-
-#(Ab*Ab - Bb*Bb)/2 * (C01)
-# (Ab / Bb) + ((Ab*Ab - Bb*Bb) / (2.0*Bb*Bb)) * R
-
-def I_numeric(r_val):
-    # f = lambda x: 1 / (1.0 + a_val**2*(1.0 - x)/2)**2
-    f = lambda x: -1.0 / (1.0 + 0.5 * (r_val**2) * (1.0 - x))**2
+def I0_numeric(r):
+    f = lambda x: 1.0 / (1.0 + 0.5 * r*r * (1.0 - x))**2
     val, _ = quad(f, -1.0, 1.0, limit=200)
     return val
 
-I_vals = np.array([I_numeric(rv) for rv in r])
-num_C1_from_I = I_vals
+def I1_numeric(r):
+    f = lambda x: x / (1.0 + 0.5 * r*r * (1.0 - x))**2
+    val, _ = quad(f, -1.0, 1.0, limit=200)
+    return val
 
-plt.figure()
-# plt.plot(A1, C1_big_form2, label=r"$C1_{\mathrm{big}}$ (log/reciprocal in $A_1$)")
-plt.plot(A1, C01, '--', label=r"$C1_{\mathrm{big}}$ (A,B,R form)")
-plt.plot(A1, num_C1_from_I, ':', label=r"$a^2\,I(a)$ (numerical integration)")
-plt.xlabel(r"$A_1$")
-plt.ylabel("Value")
-plt.title(r"Comparison: $C1_{\mathrm{big}}$ (two forms) vs $a^2 I(a)$")
-plt.grid(True)
+def C1_numeric(r):
+    I0 = I0_numeric(r)
+    I1 = I1_numeric(r)
+    return I1 / I0
+
+def C1_analytic(r):
+    A = 1.0 + 0.5*r*r
+    B = 0.5*r*r
+
+    R = (A - B) / (A + B)
+    return (A/B) + ((A*A - B*B)/(2.0*B*B)) * np.log(R)
+
+C1_num = np.array([C1_numeric(r) for r in r_vals])
+C1_an  = C1_analytic(r_vals)
+
+plt.plot(r_vals, C1_num, 'r-',  label='numeric C1 = I1/I0')
+plt.plot(r_vals, C1_an,  'b--', label='analytic C1')
+plt.xlabel(r'$v/w = r$')
+plt.ylabel(r'$C_1$')
 plt.legend()
+plt.tight_layout()
 plt.show()
