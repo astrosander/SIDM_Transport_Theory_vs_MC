Fix sign errors in dx calculations
Warming up numba JIT functions...
Done warming up.
Running no-loss-cone simulation with 5000 stars, numba-parallel, 40 blocks...
Running full-loss-cone simulation with 5000 stars, numba-parallel, 40 blocks...

 x-bin    g0(x)   gbar(no LC)   err(no LC)   gbar(LC)   err(LC)
   0.225    1.165         0.753         0.055         0.773         0.033
   0.303    1.215         1.499         0.113         1.528         0.046
   0.495    1.322         2.570         0.173         2.590         0.058
    1.04    1.532         2.019         0.154         1.996         0.052
    1.26    1.599         0.815         0.069         0.810         0.040
    1.62    1.695         0.870         0.070         0.828         0.035
    2.35    1.856         0.552         0.046         0.519         0.024
       5    2.239         0.161         0.021         0.145         0.015
     7.2    2.456         0.070         0.016         0.057         0.012
    8.94    2.598         0.055         0.009         0.044         0.009
    12.1    2.814         0.075         0.016         0.055         0.008
    19.7    3.206         0.104         0.017         0.063         0.011
    41.6    3.965         0.079         0.013         0.045         0.011
    50.3    4.186         0.046         0.010         0.020         0.006
    64.6    4.504         0.068         0.014         0.029         0.006
    93.6    5.040         0.155         0.015         0.053         0.009
     198    6.415         0.211         0.032         0.047         0.009
     287    7.306         0.130         0.030         0.023         0.007
     356    7.896         0.136         0.041         0.016         0.006
     480    8.842         0.265         0.098         0.022         0.008
     784   10.718         0.416         0.217         0.021         0.011
1.65e+03   14.603         0.214         0.182         0.003         0.003
   2e+03   15.798         0.043         0.049         0.000    

Shapiro1978/apple-to-apple/fig3/fig3_3.py
@@ -51,7 +51,7 @@ G0_TAB = np.array([
 
 NEG_E1 = np.array([
     [1.41e-1, 1.40e-1, 1.33e-1, 1.29e-1, 1.29e-1],
-    [1.47e-3, -4.67e-3, -6.33e-3, -5.52e-3, -4.78e-3],
+    [-1.47e-3, -4.67e-3, -6.33e-3, -5.52e-3, -4.78e-3],
     [1.96e-3, 1.59e-3, 2.83e-3, 3.38e-3, 3.49e-3],
     [3.64e-3, 4.64e-3, 4.93e-3, 4.97e-3, 4.98e-3],
     [8.39e-4, 8.53e-4, 8.56e-4, 8.56e-4, 8.56e-4],
@@ -384,7 +384,7 @@ def evolve_block_numba(x, j, cycle, u_tag, weight,
 
             y1, y2 = correlated_normals_numba(zeta2_star, eps2_star, j2_star)
 
-            dx = nstep * eps1_star + math.sqrt(nstep) * eps2_star * y1
+            dx = -(nstep * eps1_star + math.sqrt(nstep) * eps2_star * y1)
             x_new = xi + dx
 
             use_2d_walk = False
@@ -569,7 +569,7 @@ def run_simulation(
 
             y1, y2 = correlated_normals(zeta2_star, eps2_star, j2_star)
 
-            dx = n * eps1_star + math.sqrt(n) * eps2_star * y1
+            dx = -(n * eps1_star + math.sqrt(n) * eps2_star * y1)
             x_new = x + dx
 
             dj = n * j1_star + math.sqrt(n) * j2_star * y2
@@ -686,7 +686,7 @@ def process_stars_block(args):
 
             y1, y2 = correlated_normals(zeta2_star, eps2_star, j2_star)
 
-            dx = n * eps1_star + math.sqrt(n) * eps2_star * y1
+            dx = -(n * eps1_star + math.sqrt(n) * eps2_star * y1)
             x_new = x + dx
 
             dj = n * j1_star + math.sqrt(n) * j2_star * y2